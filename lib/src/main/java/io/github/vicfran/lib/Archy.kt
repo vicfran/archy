5d5993ee46cc6cd84e4b7f65aac13300d451782b
Victor de Francisco <vfrancisco@idealista.com>
2018-09-11 18:57:12 +0200

    Build default configuration for Realm instance

--- lib/src/main/java/io/github/vicfran/lib/Archy.kt
+++ lib/src/main/java/io/github/vicfran/lib/Archy.kt
@@ -2,11 +2,13 @@ package io.github.vicfran.lib
 
 import android.content.Context
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class Archy(context: Context) {
 
     init {
         Realm.init(context)
+        Realm.setDefaultConfiguration(RealmConfiguration.Builder().build())
     }
 
 }
